cd636c33df31bea42e5fb2e80137f9a0a26a35d1
olive <oliver-z-jonsson@hotmail.com>
2023-02-12 19:15:24 +0100

    first

--- src/Client/app.java
+++ src/Client/app.java
@@ -1,4 +1,24 @@
 package Client;
 
+import Document.Document;
+import Document.DocumentBuilder;
+import Document.DocumentFactory;
+
 public class app {
+    public static void main(String[] args){
+
+        DocumentFactory factory = new DocumentFactory();
+        DocumentBuilder documentBuilder = new DocumentBuilder(factory);
+
+        documentBuilder.addHeader("")
+                .addParagraph("")
+                .addPlainText("")
+                .addFooter("")
+                .build()
+                .getList()
+                .forEach(e ->{System.out.println(e);});
+
+
+
+    }
 }

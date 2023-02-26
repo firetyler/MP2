4b7a2c4490f183bb8b9cf308a5ac799f499e4d0a
olive <oliver-z-jonsson@hotmail.com>
2023-02-26 19:23:00 +0100

    first

--- src/Document/Facade.java
+++ src/Document/Facade.java
@@ -5,9 +5,10 @@ public class Facade {
     public DocumentBuilder getFacade(String input){
         if(input == "html")
             return new DocumentBuilder(new DocumentFactory(new HTMLDocument()));
+        else if(input == "markDown")
+            return new DocumentBuilder(new DocumentFactory(new Markdown()));
         else
             return null;
-
     }
 
 }
